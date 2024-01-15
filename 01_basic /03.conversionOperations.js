293510e6bdf5ea4f7dc8c2190cb0b2fa394111a2
Gourav Singh <146060715+Gouravsingh9755@users.noreply.github.com>
2024-01-15 19:58:59 +0000

    updating operators note

--- 01_basic /03.conversionOperations.js
+++ 01_basic /03.conversionOperations.js
@@ -27,11 +27,74 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+
+
 // *********************** Operations ***********************
+
+
+// In JavaScript, the term "operations" can refer to a variety of actions or tasks performed on data, variables, or objects. Here are some common types of operations in JavaScript:
+
+// 1. **Arithmetic Operations:**
+//    - Addition (`+`): Adds two values.
+//    - Subtraction (`-`): Subtracts the right operand from the left operand.
+//    - Multiplication (`*`): Multiplies two values.
+//    - Division (`/`): Divides the left operand by the right operand.
+//    - Modulus (`%`): Returns the remainder of the division of the left operand by the right operand.
+
+//    Example: ```javascript
+//    let result = 10 + 5; // result is 15
+//    ```
+
+// 2. **Assignment Operations:**
+//    - Assignment (`=`): Assigns the value on the right to the variable on the left.
+
+//    Example:  ```javascript
+//    let x = 10; // assigns the value 10 to variable x
+//    ```
+
+// 3. **Comparison Operations:**
+//    - Equality (`==` or `===`): Checks if two values are equal.
+//    - Inequality (`!=` or `!==`): Checks if two values are not equal.
+//    - Greater than (`>`), Greater than or equal to (`>=`), Less than (`<`), Less than or equal to (`<=`): Compare numeric values.
+
+//    Example:  ```javascript
+//    let isEqual = (5 === "5"); // isEqual is false (strict equality)
+//    ```
+
+// 4. **Logical Operations:**
+//    - Logical AND (`&&`), Logical OR (`||`): Combine boolean values.
+//    - Logical NOT (`!`): Negates a boolean value.
+
+//    Example:  ```javascript
+//    let result = (true && false); // result is false
+//    ```
+
+// 5. **String Operations:**
+//      Concatenation (`+`): Combines two strings.
+
+//    Example:  ```javascript
+//    let greeting = "Hello" + " " + "World"; // greeting is "Hello World"
+//    ```
+
+// 6. **Typeof Operation:**
+//    - `typeof`: Returns a string representing the type of a variable.
+
+//    Example:   ```javascript
+//    let type = typeof 42; // type is "number"
+//    ```
+
+// These are just a few examples, and JavaScript supports a wide range of operations for various data types and purposes. Understanding these operations is crucial for writing effective and expressive JavaScript code.
+
+
 
 let value = 3
 let negValue = -value
 // console.log(negValue);
+
+
+
+//example of operations 👇👇👇
+
 
 // console.log(2+2);
 // console.log(2-2);
