beff5049fc71d82528c75e727f1b98a43bd5325a
Gourav Singh <146060715+Gouravsingh9755@users.noreply.github.com>
2024-01-21 12:57:18 +0000

    learned more properties of functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,14 +14,15 @@ function functionName(parameters) {
   }
   
   // Function expression
-  const functionName = function(parameters) {
-    // code to be executed
-  };
+
+//   const functionName = function(parameters) {
+//     // code to be executed
+//   };
   
   // Arrow function (ES6+)
-  const functionName = (parameters) => {
-    // code to be executed
-  };
+//   const functionName = (parameters) => {
+//     // code to be executed
+//   };
   
 //   Let's understand each part: 👇👇👇👇
 
@@ -29,9 +30,9 @@ function functionName(parameters) {
 
 // 1️⃣ Function Declaration:   👈👈👈
 
-    function functionName(parameters) {
-        // code to be executed
-    }
+    // function functionName(parameters) {
+    //     // code to be executed
+    // }
   
 // 👆 function: The keyword used to declare a function.
 
@@ -48,9 +49,9 @@ function functionName(parameters) {
 
 //  2️⃣ Function Expression:            👈👈👈
 
-     const functionName = function(parameters) {
-              // code to be executed
-      };
+    //  const functionName = function(parameters) {
+    //           // code to be executed
+    //   };
 
   
 //   👆  const functionName: The function is assigned to a variable. This is known as a
@@ -62,9 +63,9 @@ function functionName(parameters) {
 
 // 3️⃣ Arrow Function or Fat Arrow Function (ES6+):  () => {}   👈👈👈
 
-      const functionName = (parameters) => {
-             // code to be executed
-      };
+    //   const functionName = (parameters) => {
+    //          // code to be executed
+    //   };
 
 
 // 👆  Introduced in ECMAScript 2015 (ES6), arrow functions provide a more concise syntax.
@@ -88,7 +89,7 @@ function functionName(parameters) {
 
 // Calling a function
 
-functionName(argument1, argument2);
+// functionName(argument1, argument2); 
 
 
 // JavaScript functions can give back results using the "return" 👈   keyword. 🔍🔍🔍
@@ -97,7 +98,7 @@ functionName(argument1, argument2);
 //  but that's not so common.
 
 
-// examples of different functions 
+// examples of different functions 👇👇👇
 
 
 function sayMyName(){
@@ -116,56 +117,259 @@ function sayMyName(){
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
-}
-
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
+// output => Result:  undefined  👈  👈  👈  
 
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
+// if you did not use " return " it gives undefined 👆 👆 👆 
+
+// _________________________________________________________________________________________
+
+// 🔻 And if you use " return " in any function next lines code will not be run in that function.  So always use " return " in the last line inside any function 🔍🔍🔍
+
+// and if you use " return " it gives the value  👇👇👇
+
+
+
+// function addTwoNumbers(number1, number2){
+
+// to wirte return code method 1️⃣ 👇
+
+//     // let result = number1 + number2
+//     // return result
+
+
+// to wirte return code method 2️⃣ 👇 in this method you save a line and without 
+// creating a variable 
+
+//     return number1 + number2
+
+// }
+
+//❗❗❗ when you return a value from any function you have to store it in new variable to 
+// access its value or consolie.log will work. 🔍🔍🔍 
+
+// const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+// output => Result:  8       👈 👈 👈  
+
+
+// __________________________________________________________________________________________
+
+// created login user function example  👇👇👇
+
+
+function loginUserMessage(username){
+
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+// output=> hitesh just logged in 👈👈👈
 
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
-}
+//🔻 If you did not passed any value through argument  while accessing the function it gives 
+// output => undefined 🔍🔍🔍  
+
+// console.log(loginUserMessage()) 
+// output=> undefined just logged in 👈👈👈
+
+
+// in this case we have to apply conditions if - else to check  ❗❗❗ 
+
+// undefined and empty string " " are considered as false value in js....🔍🔍🔍
+
+// method 1️⃣ through if(username ===  undefined)...
+
+// function loginUserMessage(username){
+//     if(username ===  undefined){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage()) 
+
+//------------------------------------------------------------------------------------------
+
+
+
+// method 2️⃣ through if(!username)... 
+
+// ! this sign changes the current value. true convert in false or false converted in true..
+// working of both methods are same..🔍🔍🔍
+
+
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return   
+//     }
+//     return `${username} just logged in`
+
+// }
+
+// console.log(loginUserMessage()) 
+
+
+
+// ------------------------------------------------------------------------------------------
+//                              ❗❗❗   New-Topic on functions  ❗❗❗
+
+
+// when you want to create a billing function for a shopping website or similer site where you don't know how much items did the customer will add in his/her cart or similarly you do not know the quatity of prizes of the items so in this case what do we do❓❓❓ 
+
+// in this situation we have to use the rest operators for ex: ( ...paraMeterName ) 
+
+
+// important points Read 👇👇🔍🔍🔍
+
+// one more thing rest operator and spread operator both are looking same (3dots + paraname)
+
+// for example => spread operator ( ...parameterName ) Rest operator ( ...parameterName )
+
+// but we can decide at the time of use that which one we are using...  ❗❗❗
+
+// for example 👇👇👇
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2));   
+// output=> 2  if user pass a single value in argument it is working properly...
+
+// ------------------------------------------------------------------------------------------
+
+// but what if user pass 2 or more arguements in it❓❓❓
+
+
+// for example 👇👇👇
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2,4,6,8));   
+// output=> 2  it gives only 2 which is the first value of the arguement. ❌❌❌ not working.
+
+
+// --------------------------------------------------------------------------------------
+
+
+//🔻now we have to use the Rest operator to accept multiple arguments 🔍🔍👇👇👇
+
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
+// output=> in an Array [ 200, 400, 500, 2000 ] which is eazy to calculate whole the values.
+//  Now its working perfectly..✅✅✅
+
+// --------------------------------------------------------------------------------------------
+
+//                             ❗❗❗    interview question   ❗❗❗
+
+// what if we use Rest operator after 1 or 2 or more paramenters then 
+// what will be the output❓❓❓
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+// output=> [ 500, 2000 ]  👈👈👈
+// because ' 200 ' is passed into ' val1 ' and similarly '400' is passed into ' val2 ' 🔍🔍🔍
+// and rest of the values passed into the rest operator ...num1
+
+
+// --------------------------------------------------------------------------------------
+
+
+// How to pass objects through parameter and arguments in a function ❓❓❓
+
+// method 1️⃣
+
 const user = {
     username: "hitesh",
-    prices: 199
+    price: 199
 }
 
 function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+    // inside the function we have to use the name which we have taken in paramenter insteed the actual name of the object to take the values. and if you take the actual name then it will be okkk. 🔍🔍❗❗
+
 }
 
-// handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
+handleObject(user)
 
-const myNewArray = [200, 400, 100, 600]
+// we can use " anyobject " at the place of parameter to access koibhi object. it is ok ✅✅
+// but at the time of accessing the values we have to use the name of the 
+// object which we are reffering 🔍🔍🔍❗❗
 
-function returnSecondValue(getArray){
-    return getArray[1]
-}
+// method 2️⃣  we can also pass the values of an object through the agruments...
+
+// for example 👇👇👇
+
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+
+
+// ----------------------------------------------------------------------------------------
+
+
+// How to pass Arrays through parameter and arguments in a function ❓❓❓
+
+
+// create a function which accepts the value of an array and return its 2nd value ❓❓❓
+
+// for example 👇👇👇
+
+// method 1️⃣ pass through variable..
+
+// const myNewArray = [200, 400, 100, 600]
+
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+
+// //  As we know the index value starts with [0], So we have
+// //  to take [1] behalf of the index number for accessing the second value from the Array. 
+
+// we can use " getArray " at the place of parameter to access any Array. it is ok ✅✅✅
+// but at the time of accessing the values we have to use the name of the 
+// object which we are reffering 🔍🔍🔍❗❗
+
+
+// }
 
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+
+// output=> 400 [ we got the 2nd value ]  👈👈👈
+
+
+// method 2️⃣  we can also pass the values through the agruments of an Array...
+
+// for example 👇👇👇
+
+
+// console.log(returnSecondValue([200, 400, 500, 1000]));
+
+// output=> 400 [ we got the 2nd value ]  👈👈👈
+
